d2cdbcd4b5f4cc8624272e39c0af87022259eaf2
Liang <liangtang@liangtang-macbookpro.roam.corp.google.com>
2017-01-09 17:50:14 -0800

    small changes for daily job

--- run_daily.py
+++ run_daily.py
@@ -1,8 +1,11 @@
 import sys
 import os
+import datetime
 
+print "The daily job started at "+str(datetime.datetime.now())
 print "Current directory is "+os.getcwd()
 sys.path.append(os.getcwd())
 
 import pyvalue.jobs as jobs
 jobs.update_sp500_yahoofinance_stock_quote()
+print "The daily job finished at "+str(datetime.datetime.now())
