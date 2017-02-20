6eed59360d6e2fabf1fe1d590449bce8a1c6af2e
Liang <liangtang@liangtang-macbookpro.roam.corp.google.com>
2017-02-20 13:15:56 -0800

    Add dividend pay date and ex-diviend date into yahoo daily quote

--- run_daily.py
+++ run_daily.py
@@ -8,7 +8,7 @@ print "Current directory is "+os.getcwd()
 sys.path.append(os.getcwd())
 
 import pyvalue.jobs as jobs
-#jobs.update_sp500_yahoofinance_stock_quote()
+jobs.update_sp500_yahoofinance_stock_quote()
 jobs.update_nasdaq_etf_yahoofinance_stock_quote()
 print "====================="
 print str(datetime.datetime.now())+": the daily job has finished"
